392634e5ea864f2f10f58e2d9ddafbe90c62937a
Syed0910 <syednuman094@gmail.com>
2025-09-02 10:44:57 +0530

    included theme file

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ThemeProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
